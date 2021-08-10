91d4ec73e5c5db5f430e9a59c3eb0753961e5a13
Jirot Chaitamart <42835752+Joe16723PP@users.noreply.github.com>
2021-08-10 15:38:00 +0700

    add user unit test

--- test/user.js
+++ test/user.js
@@ -1,9 +1,43 @@
-const { expect } = require("chai");
-const sinon = require("sinon");
+const chai = require("chai");
+const chaiHttp = require("chai-http");
 
-const userRoutes = require("../dist/routes/user");
+const app = require("../dist/app").default;
+chai.use(chaiHttp);
+chai.should();
 
 describe("User routes", () => {
-  it("should error if no auth bearer token", () => {});
-  it("should error if length of order is 0", () => {});
+  it("should success with user data and status 200", (done) => {
+    chai
+      .request(app)
+      .post("/sign-in")
+      .set("content-type", "application/json")
+      .send({ username: "tester01", password: "tester01" })
+      .end((err, res) => {
+        const { token } = res.body;
+        chai
+          .request(app)
+          .get("/users")
+          .set({ Authorization: `Bearer ${token}` })
+          .end((err, res) => {
+            console.log(res.body);
+            res.should.have.status(200);
+            res.body.should.have.property("name");
+            res.body.should.have.property("surname");
+            res.body.should.have.property("date_of_birth");
+            res.body.should.have.property("orders");
+            done();
+          });
+      });
+  });
+  it("should error if invalid bearer token", (done) => {
+    const token = "sometoken";
+    chai
+      .request(app)
+      .get("/users")
+      .set({ Authorization: `Bearer ${token}` })
+      .end((err, res) => {
+        res.should.have.status(401);
+        done();
+      });
+  });
 });
